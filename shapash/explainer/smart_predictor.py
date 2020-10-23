7fed383e30c4a267680871f4c226870552fe1f1a
MARTIN Johann <johann.martin@externe.maif.fr>
2020-10-23 10:14:30 +0200

    Add mask_params into SmartPredictor Attributes

--- shapash/explainer/smart_predictor.py
+++ shapash/explainer/smart_predictor.py
@@ -43,7 +43,7 @@ class SmartPredictor :
     _classes : list, None
         List of labels if the model used is for classification problem, None otherwise.
     mask_params : dict (optional)
-            Dictionnary allowing the user to define a apply a filter to summarize the local explainability.
+        Dictionnary allowing the user to define a apply a filter to summarize the local explainability.
 
     How to declare a new SmartPredictor object?
 
