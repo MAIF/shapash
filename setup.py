4f2045071f6d65cc7ae13afc6fdcfe3c1cd3562f
Thibaud <trealdelsarte@quantmetry.com>
2021-06-01 13:59:18 +0200

    add category_encoders in extras require

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ extras['xgboost'] = ['xgboost>=1.0.0']
 extras['lightgbm'] = ['lightgbm>=2.3.0']
 extras['catboost'] = ['catboost>=0.21']
 extras['scikit-learn'] = ['scikit-learn>=0.23.0']
+extras['category_encoders'] = ['category_encoders==2.2.2']
 
 setup_requirements = ['pytest-runner', ]
 
