cb6e876bca7a8a88113204d52331ad24da8e2a02
Thibaud <trealdelsarte@quantmetry.com>
2021-06-18 15:20:55 +0200

    fix python version as only python 3.9.0 was available

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ test_requirements = ['pytest', ]
 setup(
     name="shapash",
     version="1.4.1",
-    python_requires='>3.5, <=3.9',
+    python_requires='>3.5, <3.10',
     url='https://github.com/MAIF/shapash',
     author="Yann Golhen, Sebastien Bidault, Yann Lagre, Maxime Gendre",
     author_email="yann.golhen@maif.fr",
