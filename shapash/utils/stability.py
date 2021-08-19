b0fc78ce1e023c94debdfec045615e2d26ac4901
Francesco-Marini <francesco.marini@berkeley.edu>
2021-08-19 10:30:14 +0200

    added utils methods

---
+++ shapash/utils/stability.py
@@ -0,0 +1,287 @@
+import numpy as np
+import pandas as pd
+import plotly.colors
+import re
+from sklearn.preprocessing import normalize
+
+
+def _df_to_array(instances):
+    """Transform inputs into arrays
+
+    Parameters
+    ----------
+    instances : DataFrame, Series or array
+        Input data
+
+    Returns
+    -------
+    instances : array
+        Transformed features
+    """
+    if isinstance(instances, pd.DataFrame):
+        return instances.values
+    elif isinstance(instances, pd.Series):
+        return np.array([instances.values])
+    else:
+        return instances
+
+
+def _compute_distance(x1, x2, mean_vector, epsilon=0.0000001):
+    """Compute distances between data points by using L1 on normalized data : sum(abs(x1-x2)/(mean_vector+epsilon))
+
+    Parameters
+    ----------
+    x1 : array
+        First vector
+    x2 : array
+        Second vector
+    mean_vector : array
+        Each value of this vector is the std.dev for each feature in dataset
+
+    Returns
+    -------
+    diff : float
+        Returns :math:`\\sum(\\frac{|x1-x2|}{mean\_vector+epsilon})`
+    """
+    diff = np.sum(np.abs(x1 - x2) / (mean_vector + epsilon))
+    return diff
+
+
+def _compute_similarities(instance, dataset):
+    """Compute pairwise distances between an instance and all other data points
+
+    Parameters
+    ----------
+    instance : 1D array
+        Reference data point
+    dataset : 2D array
+        Entire dataset used to identify neighbors
+
+    Returns
+    -------
+    similarity_distance : array
+        V[j] == distance between actual instance and instance j
+    """
+    mean_vector = np.array(dataset, dtype=np.float32).std(axis=0)
+    similarity_distance = np.zeros(dataset.shape[0])
+
+    for j in range(0, dataset.shape[0]):
+        # Calculate distance between point and instance j
+        dist = _compute_distance(instance, dataset[j], mean_vector)
+        similarity_distance[j] = dist
+
+    return similarity_distance
+
+
+def _get_radius(dataset, n_neighbors, sample_size=500, percentile=95):
+    """Calculate the maximum allowed distance between points to be considered as neighbors
+
+    Parameters
+    ----------
+    dataset : DataFrame
+        Pool to sample from and calculate a radius
+    n_neighbors : int
+        Maximum number of neighbors considered per instance
+    sample_size : int, optional
+        Number of data points to sample from dataset, by default 500
+    percentile : int, optional
+        Percentile used to calculate the distance threshold, by default 95
+
+    Returns
+    -------
+    radius : float
+        Distance threshold
+    """
+    # Select 500 points max to sample
+    size = min([dataset.shape[0], sample_size])
+    # Randomly sample points from dataset
+    sampled_instances = dataset[np.random.randint(0, dataset.shape[0], size), :]
+    # Define normalization vector
+    mean_vector = np.array(dataset, dtype=np.float32).std(axis=0)
+    # Initialize the similarity matrix
+    similarity_distance = np.zeros((size, size))
+    # Calculate pairwise distance between instances
+    for i in range(size):
+        for j in range(i, size):
+            dist = _compute_distance(sampled_instances[i], sampled_instances[j], mean_vector)
+            similarity_distance[i, j] = dist
+            similarity_distance[j, i] = dist
+    # Select top n_neighbors
+    ordered_X = np.sort(similarity_distance)[:, 1: n_neighbors + 1]
+    # Select the value of the distance that captures XX% of all distances (percentile)
+    return np.percentile(ordered_X.flatten(), percentile)
+
+
+def find_neighbors(selection, dataset, model, mode, n_neighbors=10):
+    """For each instance, select neighbors based on 3 criteria:
+
+    1. First pick top N closest neighbors (L1 Norm + st. dev normalization)
+    2. Filter neighbors whose model output is too different from instance (see condition below)
+    3. Filter neighbors whose distance is too big compared to a certain threshold
+
+    Parameters
+    ----------
+    selection: array
+        Indices of rows to be displayed on the stability plot
+    dataset : DataFrame
+        Entire dataset used to identify neighbors
+    model : model object
+        ML model
+    mode : str
+        "classification" or "regression"
+    n_neighbors : int, optional
+        Top N neighbors initially allowed, by default 10
+
+    Returns
+    -------
+    all_neighbors : list of 2D arrays
+        Wrap all instances with corresponding neighbors in a list with length (#instances).
+        Each array has shape (#neighbors, #features) where #neighbors includes the instance itself.
+    """
+    instances = _df_to_array(dataset.loc[selection])
+    dataset = _df_to_array(dataset)
+
+    all_neighbors = np.empty((0, instances.shape[1] + 1), float)
+    """Filter 1 : Pick top N closest neighbors"""
+    for instance in instances:
+        c = _compute_similarities(instance, dataset)
+        # Pick indices of the closest neighbors (and include instance itself)
+        neighbors_indices = np.argsort(c)[: n_neighbors + 1]
+        # Return instance with its neighbors
+        neighbors = dataset[neighbors_indices]
+        # Add distance column
+        neighbors = np.append(neighbors, c[neighbors_indices].reshape(n_neighbors + 1, 1), axis=1)
+        all_neighbors = np.append(all_neighbors, neighbors, axis=0)
+
+    # Calculate predictions for all instances and corresponding neighbors
+    if mode == "regression":
+        # For XGB it is necessary to add columns in df, otherwise columns mismatch (Removed)
+        predictions = model.predict(all_neighbors[:, :-1])
+    elif mode == "classification":
+        predictions = model.predict_proba(all_neighbors[:, :-1])[:, 1]
+
+    # Add prediction column
+    all_neighbors = np.append(all_neighbors, predictions.reshape(all_neighbors.shape[0], 1), axis=1)
+    # Split back into original chunks (1 chunck = instance + neighbors)
+    all_neighbors = np.split(all_neighbors, instances.shape[0])
+
+    """Filter 2 : neighbors with similar blackbox output"""
+    # Remove points if prediction is far away from instance prediction
+    if mode == "regression":
+        # Trick : use enumerate to allow the modifcation directly on the iterator
+        for i, neighbors in enumerate(all_neighbors):
+            all_neighbors[i] = neighbors[abs(neighbors[:, -1] - neighbors[0, -1]) < 0.1 * abs(neighbors[0, -1])]
+    elif mode == "classification":
+        for i, neighbors in enumerate(all_neighbors):
+            all_neighbors[i] = neighbors[abs(neighbors[:, -1] - neighbors[0, -1]) < 0.1]
+
+    """Filter 3 : neighbors below a distance threshold"""
+    # Remove points if distance is bigger than radius
+    radius = _get_radius(dataset, n_neighbors)
+
+    for i, neighbors in enumerate(all_neighbors):
+        # -2 indicates the distance column
+        all_neighbors[i] = neighbors[neighbors[:, -2] < radius]
+    return all_neighbors
+
+
+def shap_neighbors(instance, dataset, contributions):
+    """For an instance and corresponding neighbors, calculate various
+    metrics (described below) that are useful to evaluate local stability
+
+    Parameters
+    ----------
+    instance : 2D array
+        Instance + neighbours with corresponding features
+    dataset : DataFrame
+        Entire dataset used to identify neighbors
+    contributions : DataFrame
+        Calculated SHAP values for the dataset
+
+    Returns
+    -------
+    norm_shap_values : array
+        Normalized SHAP values (with corresponding sign) of instance and its neighbors
+    average_diff : array
+        Variability (stddev / mean) of normalized SHAP values (using L1) across neighbors for each feature
+    norm_abs_shap_values[0, :] : array
+        Normalized absolute SHAP value of the instance
+    """
+    # Extract SHAP values for instance and neighbors
+    # :-2 indicates that two columns are disregarded : distance to instance and model output
+    ind = pd.merge(dataset.reset_index(), pd.DataFrame(instance[:, :-2], columns=dataset.columns), how='inner')\
+        .set_index(dataset.index.name if dataset.index.name is not None else 'index').index
+    shap_values = contributions.loc[ind]
+    # For neighbors comparison, the sign of SHAP values is taken into account
+    norm_shap_values = normalize(shap_values, axis=1, norm="l1")
+    # But not for the average impact of the features across the dataset
+    norm_abs_shap_values = normalize(np.abs(shap_values), axis=1, norm="l1")
+    # Compute the average difference between the instance and its neighbors
+    average_diff = norm_shap_values.std(axis=0) / norm_abs_shap_values.mean(axis=0)
+    # Replace NaN with 0
+    average_diff = np.nan_to_num(average_diff)
+
+    return norm_shap_values, average_diff, norm_abs_shap_values[0, :]
+
+
+def get_continuous_color(colorscale, intermed):
+    """
+    Plotly continuous colorscales assign colors to the range [0, 1]. This function computes the intermediate
+    color for any value in that range.
+
+    Plotly doesn't make the colorscales directly accessible in a common format.
+    Some are ready to use:
+
+        colorscale = plotly.colors.PLOTLY_SCALES["Greens"]
+
+    Others are just swatches that need to be constructed into a colorscale:
+
+        viridis_colors, scale = plotly.colors.convert_colors_to_same_type(plotly.colors.sequential.Viridis)
+        colorscale = plotly.colors.make_colorscale(viridis_colors, scale=scale)
+
+    :param colorscale: A plotly continuous colorscale defined with RGB string colors.
+    :param intermed: value in the range [0, 1]
+    :return: color in rgb string format
+    :rtype: str
+    """
+    if len(colorscale) < 1:
+        raise ValueError("colorscale must have at least one color")
+
+    if intermed <= 0 or len(colorscale) == 1:
+        return colorscale[0][1]
+    if intermed >= 1:
+        return colorscale[-1][1]
+
+    for cutoff, color in colorscale:
+        if intermed > cutoff:
+            low_cutoff, low_color = cutoff, color
+        else:
+            high_cutoff, high_color = cutoff, color
+            break
+
+    # noinspection PyUnboundLocalVariable
+    return plotly.colors.find_intermediate_color(
+        lowcolor=low_color, highcolor=high_color,
+        intermed=((intermed - low_cutoff) / (high_cutoff - low_cutoff)),
+        colortype="rgb")
+
+
+def get_color_rgb(colorscale_name, loc):
+    from _plotly_utils.basevalidators import ColorscaleValidator
+    # first parameter: Name of the property being validated
+    # second parameter: a string, doesn't really matter in our use case
+    cv = ColorscaleValidator("colorscale", "")
+    # colorscale will be a list of lists: [[loc1, "rgb1"], [loc2, "rgb2"], ...] 
+    colorscale = cv.validate_coerce(colorscale_name)
+    
+    if hasattr(loc, "__iter__"):
+        return [get_continuous_color(colorscale, x) for x in loc]
+    return get_continuous_color(colorscale, loc)
+
+
+def get_color_hex(colorscale_name, loc):
+    rgb_string = get_color_rgb(colorscale_name, loc)
+    rgb_float = tuple([float(s) for s in re.findall(r"\d+\.\d+", rgb_string)])
+    rgb_int = tuple([round(val) for val in rgb_float])
+    hex = '#%02x%02x%02x' % rgb_int
+    return hex
