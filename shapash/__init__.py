1bf8f94795d3f6d2b8cb1415ef5bcf0df9ca2e94
ThomasBouche <thomas.bouche@maif.fr>
2022-03-07 15:18:48 +0100

    bump version to 1.6.2

--- shapash/__init__.py
+++ shapash/__init__.py
@@ -3,4 +3,4 @@
 
 __author__ = """Yann Golhen, Yann Lagré, Sebastien Bidault, Maxime Gendre, Thomas Bouche, Johann Martin, Guillaume Vignal"""
 __email__ = 'yann.golhen@maif.fr, yann.lagre@maif.fr, sebabstien.bidault.marketing@maif.fr, thomas.bouche@maif.fr, guillaume.vignal@maif.fr'
-__version__ = '1.6.1'
+__version__ = '1.6.2'
