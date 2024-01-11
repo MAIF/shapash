326e47a57c403c2d8bd1460e9ccad26ef14f2c0e
Clément Guérin <clement.guerin@maif.fr>
2024-01-11 11:38:39 +0100

    clean backend code base

--- shapash/backend/shap_backend.py
+++ shapash/backend/shap_backend.py
@@ -1,5 +1,5 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 import shap
 
 from shapash.backend.base_backend import BaseBackend
@@ -8,18 +8,18 @@ from shapash.backend.base_backend import BaseBackend
 class ShapBackend(BaseBackend):
     # When grouping features contributions together, Shap uses the sum of the contributions
     # of the features that belong to the group
-    column_aggregation = 'sum'
-    name = 'shap'
+    column_aggregation = "sum"
+    name = "shap"
 
     def __init__(self, model, preprocessing=None, masker=None, explainer_args=None, explainer_compute_args=None):
-        super(ShapBackend, self).__init__(model, preprocessing)
+        super().__init__(model, preprocessing)
         self.masker = masker
         self.explainer_args = explainer_args if explainer_args else {}
         self.explainer_compute_args = explainer_compute_args if explainer_compute_args else {}
 
         if self.explainer_args:
             if "explainer" in self.explainer_args.keys():
-                shap_parameters = {k: v for k, v in self.explainer_args.items() if k != 'explainer'}
+                shap_parameters = {k: v for k, v in self.explainer_args.items() if k != "explainer"}
                 self.explainer = self.explainer_args["explainer"](**shap_parameters)
             else:
                 self.explainer = shap.Explainer(**self.explainer_args)
@@ -31,14 +31,13 @@ class ShapBackend(BaseBackend):
             elif shap.explainers.Additive.supports_model_with_masker(model, self.masker):
                 self.explainer = shap.Explainer(model=model, masker=self.masker)
             # otherwise use a model agnostic method
-            elif hasattr(model, 'predict_proba'):
+            elif hasattr(model, "predict_proba"):
                 self.explainer = shap.Explainer(model=model.predict_proba, masker=self.masker)
-            elif hasattr(model, 'predict'):
+            elif hasattr(model, "predict"):
                 self.explainer = shap.Explainer(model=model.predict, masker=self.masker)
             # if we get here then we don't know how to handle what was given to us
             else:
                 raise ValueError("The model is not recognized by Shapash! Model: " + str(model))
-
 
     def run_explainer(self, x: pd.DataFrame) -> dict:
         """
@@ -78,8 +77,10 @@ def get_shap_interaction_values(x_df, explainer):
         Shap interaction values for each sample as an array of shape (# samples x # features x # features).
     """
     if not isinstance(explainer, shap.TreeExplainer):
-        raise ValueError(f"Explainer type ({type(explainer)}) is not a TreeExplainer. "
-                         f"Shap interaction values can only be computed for TreeExplainer types")
+        raise ValueError(
+            f"Explainer type ({type(explainer)}) is not a TreeExplainer. "
+            f"Shap interaction values can only be computed for TreeExplainer types"
+        )
 
     shap_interaction_values = explainer.shap_interaction_values(x_df)
 
