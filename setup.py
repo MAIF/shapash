424793d37b265012a9cd63262e38bb10a6797993
MaximeLecardonnel6x7 <mlecardo@hotmail.fr>
2024-03-07 14:55:26 +0100

    Fixes for scikit-learn new versions.

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ requirements = [
     "dash-table>=5.0.0",
     "nbformat>4.2.0",
     "numba>=0.53.1",
-    "scikit-learn>=1.0.1,<1.4",
+    "scikit-learn>=1.4.0",
     "category_encoders>=2.6.0",
     "scipy>=0.19.1",
 ]
